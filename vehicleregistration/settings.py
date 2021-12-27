bfc9602a2a542b74558262c656fc661610c96d88
mosestwesigye <twesigyemoses1@gmail.com>
2021-12-27 14:25:39 +0300

    Add Postgres Database Configurations

--- vehicleregistration/settings.py
+++ vehicleregistration/settings.py
@@ -79,11 +79,22 @@ WSGI_APPLICATION = 'vehicleregistration.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
+if DEBUG:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+else:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cardb',
+        'USER': 'cardbuser',
+        'PASSWORD': 'Hsdp@dm!n2019',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
