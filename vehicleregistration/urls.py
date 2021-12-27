184b4ffdbb51aa407ea8480392158b09a3f5749f
mosestwesigye <twesigyemoses1@gmail.com>
2021-12-27 14:43:23 +0300

    Updated default URL

--- vehicleregistration/urls.py
+++ vehicleregistration/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 CarApi and admin url paths
 """
 urlpatterns = [
-    path('api/', include ('cars.urls')),
+    path('', include ('cars.urls')),
     path('admin/', admin.site.urls),
     
 ]
